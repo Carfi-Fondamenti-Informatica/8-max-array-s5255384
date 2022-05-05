659eac44593e534f05496e01511e3d7caf27bb56
s5255384 <100850996+s5255384@users.noreply.github.com>
2022-05-05 17:18:35 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include"lib.h"
 
+using namespace std;
+
+float Numeri (const float array[], int dim){
+
+    float num=0;
+
+    for(int i=0;i<dim; i++){
+        if(array[i]>num)
+            num=array[i];
+    }
+
+   return num;
+}
