00c9615f8c11b649fefadbd9967b84dfe8ed7e27
s5255384 <100850996+s5255384@users.noreply.github.com>
2022-05-05 17:13:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
-#include <iostream>
+#include<iostream>
+#include"lib.h"
+
+using namespace std;
 
 int main(){
-  return 0;
+
+    int x;
+    float y,result;
+    cin>>x;
+    float array[x];
+
+    for(int i=0; i<x; i++){
+        cin>>y;
+        array[i]=y;
+    }
+    result=Numeri(array, x);
+
+    cout<<result<<endl;
+
+    return 0;
 }
